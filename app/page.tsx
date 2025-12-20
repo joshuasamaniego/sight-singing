eded8bfa3143d5e950f8334ca8089c13a3f20585
Joshua Samaniego <joshua@jiko.io>
2025-12-19 16:06:51 -0800

    testing builds

--- app/page.tsx
+++ app/page.tsx
@@ -18,6 +18,8 @@ export default function HomePage() {
 
   return (
     <main style={{ padding: 20 }}>
+      <h1>WOOOHOOO IT WORKED!</h1>
+      <span>Im so amazing :)</span>
       <ControlPanel
         tempo={tempo}
         meter={meter}
